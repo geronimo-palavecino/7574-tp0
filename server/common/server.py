17058c4823e7f94d532fc71f5ee7364418e56bcd
Geronimo <gpalavecino@fi.uba.ar>
2025-03-17 14:47:39 -0300

    Feature: Added graceful shutdown to server

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,7 @@
 import socket
 import logging
-
+import signal
+import sys
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -8,18 +9,36 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._current_connection = None
+
+        # Setting the signal handler
+        signal.signal(signal.SIGTERM, self.__sigterm_handler)
+
+    def __sigterm_handler(self, signal, frame):
+        """
+        SIGTERM signal handler
+
+        When the application receives a SIGTERM signal, all the file descriptors 
+        (welcoming socket, and client current socket) are closed for a graceful shutdown
+        """
+        self._server_socket.shutdown(socket.SHUT_RDWR)
+        self._server_socket.close()
+        logging.info(f'action: graceful_shutdown | result: success | Welcoming socket closed')
+        if self._current_connection != None:
+            self._current_connection.shutdown(socket.SHUT_RDWR)
+            self._current_connection.close()
+            logging.info(f'action: graceful_shutdown | result: success | Client socket closed')
+        sys.exit(0)
 
     def run(self):
         """
         Dummy Server loop
 
         Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
+        communication with a client. After client with communication
         finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
         while True:
             client_sock = self.__accept_new_connection()
             self.__handle_client_connection(client_sock)
@@ -42,6 +61,7 @@ class Server:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
+            self._current_connection = None
 
     def __accept_new_connection(self):
         """
@@ -54,5 +74,6 @@ class Server:
         # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         c, addr = self._server_socket.accept()
+        self._current_connection = c
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
