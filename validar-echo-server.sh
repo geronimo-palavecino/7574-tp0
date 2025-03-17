1d44a9cf11235a9cc0886c5aeb64ab7c5aa9028f
Geronimo <gpalavecino@fi.uba.ar>
2025-03-17 00:21:32 -0300

    Fix: Changet comparison operator to be POSIX ocmpatible

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MESSAGE="test_message"
+MESSAGE="testMessage"
 
 docker run -d --quiet --name server_validator --network tp0_testing_net alpine:latest tail -f /dev/null
 
 RESPONSE=$(echo "$MESSAGE" | docker exec -i server_validator nc server:12345)
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
