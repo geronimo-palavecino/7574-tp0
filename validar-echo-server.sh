d886e595effb2ce4e694c1f09eece3f8e763cbf4
Geronimo <gpalavecino@fi.uba.ar>
2025-03-16 23:32:56 -0300

    Feature: echo server validation

---
+++ validar-echo-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MESSAGE="test_message"
+
+docker run -d --quiet --name server_validator --network tp0_testing_net alpine:latest tail -f /dev/null
+
+RESPONSE=$(echo "$MESSAGE" | docker exec -i server_validator nc server:12345)
+
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
+
+docker rm -f server_validator
