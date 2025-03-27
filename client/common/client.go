9b26fafbca71982ce07dfa576a8f64c36f771da4
Geronimo <gpalavecino@fi.uba.ar>
2025-03-27 03:58:40 -0300

    Fix: Error en cliente

--- client/common/client.go
+++ client/common/client.go
@@ -12,6 +12,8 @@ import (
 	"github.com/op/go-logging"
 )
 
+const MAX_AMOUNT_TRIES = 3
+
 var log = logging.MustGetLogger("log")
 
 // ClientConfig Configuration used by the client
@@ -46,16 +48,19 @@ func NewClient(config ClientConfig) *Client {
 // failure, error is printed in stdout/stderr and exit 1
 // is returned
 func (c *Client) createClientSocket() error {
-	conn, err := net.Dial("tcp", c.config.ServerAddress)
-	if err != nil {
-		log.Criticalf(
-			"action: connect | result: fail | client_id: %v | error: %v",
-			c.config.ID,
-			err,
-		)
+	var err error
+	for tries := 1; tries <= MAX_AMOUNT_TRIES; tries++ {
+		conn, err := net.Dial("tcp", c.config.ServerAddress)
+		if err == nil {
+			c.conn = conn
+			return nil
+			
+		}
+
+		time.Sleep(time.Duration(tries * 500) * time.Millisecond)
 	}
-	c.conn = conn
-	return nil
+	
+	return err
 }
 
 // StartClientLoop Send messages to the client until some time threshold is met
@@ -75,7 +80,14 @@ func (c *Client) ClientLoop() {
 					return
 				case <- loopPeriod:
 					// Create the connection the server in every loop iteration
-					c.createClientSocket()
+					err := c.createClientSocket()
+					if err != nil {
+						log.Criticalf(
+							"action: connect | result: fail | client_id: %v | error: %v",
+							c.config.ID,
+							err,
+						)
+					}
 
 					fmt.Fprintf(
 						c.conn,
